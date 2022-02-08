ed1ef984c7b5bd56d4e3d9b04b69719508b10bfa
Paulo Guilherme <pauloguilherme@usp.br>
2022-02-07 23:41:15 -0300

    Adding the first draft of the class that represents the game and the main file.

---
+++ src/Main.py
@@ -0,0 +1,8 @@
+from Tic_tac_toe import Tic_tac_toe
+
+def main():
+    game = Tic_tac_toe(side = 3)
+    game.run()
+
+if __name__ == "__main__":
+    main()
---
+++ src/Tic_tac_toe.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+# Class to represent tic tac toe game:
+class Tic_tac_toe():
+    # Constructor:
+    def __init__(self, side = 3):
+        self.board = [[0 for i in np.arange(side)] for j in np.arange(side)] # Creating board
+        #self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.side = side # Saving variable with board length
+    
+
+    # Function to show board:
+    def show_board(self):
+        for i in np.arange(0, self.side):
+            for j in np.arange(0, self.side):
+                print(self.board[i][j], end=" ")
+            print("")
+    
+
+    # Function to test if someone won:
+    def checking_winer(self):
+
+        # Checking rows:
+        for row in np.arange(self.side):
+            symb = self.board[row][0]
+
+            if(symb == 0):
+                continue
+            
+            for symbol in np.arange(1, self.side):
+                if(symb == self.board[row][symbol]):
+                    if(symbol == self.side - 1):
+                        return True
+                    continue
+                else:
+                    break
+
+        
+        # Checking columns:
+        for col in np.arange(self.side):
+            symb = self.board[0][col]
+
+            if(symb == 0):
+                continue
+            
+            for symbol in np.arange(1, self.side):
+                if(symb == self.board[symbol][col]):
+                    if(symbol == self.side - 1):
+                        return True
+                    continue
+                else:
+                    break
+        
+
+        # Checking main diagonal:
+        for id in np.arange(self.side):
+            symb = self.board[0][0]
+
+            if(symb == 0):
+                break
+            
+            if(symb == self.board[id][id]):
+                if(id == self.side - 1):
+                    return True
+                continue
+            else:
+                break
+
+
+        # Checking other diagonal:
+        for id in np.arange(self.side):
+            symb = self.board[self.side - 1][0]
+
+            if(symb == 0):
+                break
+
+            if(symb == self.board[self.side - id - 1][id]):
+                if(id == self.side - 1):
+                    return True
+                continue
+            else:
+                break
+        
+
+        return False        
+
+
+    # Function to run for each player turn:
+    def player_turn(self, player):
+        
+        self.show_board()
+
+        print("It is {player} time: ".format(player = player), end="")
+
+        x, y = map(int, input().split())
+
+        self.board[x][y] = player
+
+        if(self.checking_winer()):
+            print("Player {player} won this game!".format(player = player))
+            self.game_on = False
+
+    
+
+    # Function to run the game:
+    def run(self):
+        self.game_on = True
+        print("Starting tic-tac-toe game!")
+
+        while(self.game_on):
+            
+            self.player_turn("x")
+            
+            if(not self.game_on):
+                break
+            
+            self.player_turn("o")
